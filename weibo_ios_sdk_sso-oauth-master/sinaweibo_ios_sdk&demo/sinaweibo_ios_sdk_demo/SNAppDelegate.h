184358bfdf0f25ebf921ff06e7ebf059306e092b
weirdln <weirdln@hotmail.com>
2013-05-03 14:02:45 +0800

    add

---
+++ weibo_ios_sdk_sso-oauth-master/sinaweibo_ios_sdk&demo/sinaweibo_ios_sdk_demo/SNAppDelegate.h
@@ -0,0 +1,43 @@
+//
+//  SNAppDelegate.h
+//  sinaweibo_ios_sdk_demo
+//
+//  Created by Wade Cheng on 4/23/12.
+//  Copyright (c) 2012 SINA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/**将下面注释取消，并定义自己的app key，app secret以及授权跳转地址uri
+ 此demo即可编译运行**/
+
+#define kAppKey             @"3201194191"//@"your app_key"
+#define kAppSecret          @"0334252914651e8f76bad63337b3b78f"//@"your app_secret"
+#define kAppRedirectURI     @"http://appgo.cn" //@"your app_rederict_uri"
+
+#ifndef kAppKey
+#error
+#endif
+
+#ifndef kAppSecret
+#error
+#endif
+
+#ifndef kAppRedirectURI
+#error
+#endif
+
+@class SinaWeibo;
+@class SNViewController;
+
+@interface SNAppDelegate : UIResponder <UIApplicationDelegate>
+{
+    SinaWeibo *sinaweibo;
+}
+
+@property (readonly, nonatomic) SinaWeibo *sinaweibo;
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) SNViewController *viewController;
+
+@end
